44b13f9b5f6d3c811fa2d717e5969b364542139b
itsmeashutosh43 <chapagainashutosh8@gmail.com>
2020-05-18 14:13:07 +0545

    dqn completed

--- dqn/exercise/model.py
+++ dqn/exercise/model.py
@@ -15,8 +15,17 @@ class QNetwork(nn.Module):
         """
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size , 32)
+        self.fc2 = nn.Linear(32,64)
+        self.fc3 = nn.Linear(64,action_size)
         "*** YOUR CODE HERE ***"
 
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        pass
+        action = F.relu(self.fc1(state))
+        action = F.relu(self.fc2(action))
+        action = self.fc3(action)
+        
+        return action
+        
+
